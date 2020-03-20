7324c48cc8aa97437a16ace08e0c701fc3e96fdb
Martijn M.W. Gribnau <garm@ilumeo.com>
2020-03-20 04:15:52 +0100

    Move parameterized macro to crates/parameterized_macro folder

--- src/lib.rs
+++ src/lib.rs
@@ -2,15 +2,15 @@ extern crate parameterized_macro;
 
 pub use parameterized_macro::parameterized;
 
-/// Attribute macro's such as 'parameterized' do not enable the run tests intent for a module
+/// Attribute parameterized_macro's such as 'parameterized' do not enable the run tests intent for a module
 /// marked as cfg(test) (or a #[test] function for that matter) in Intellij.
 ///
 /// To enable the intent within a module, we need at least a single test marked with `#[test]`.
-/// The `ide!()` macro is a work around for this issue and creates this empty test. It can be called
+/// The `ide!()` parameterized_macro is a work around for this issue and creates this empty test. It can be called
 /// within every module where we wish to run test cases using the run configuration / run test context
 /// menu.
 ///
-/// Using the intellij-rust new macro expansion engine, if this macro is called within a module,
+/// Using the intellij-rust new parameterized_macro expansion engine, if this parameterized_macro is called within a module,
 /// the module will be marked as test, and the 'run as test' context menu will be provided in the
 /// gutter.
 #[macro_export]
