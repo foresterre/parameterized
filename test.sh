95d1f93aa6e57c87f42f0acaa6eab86d7055fbb1
Martijn Gribnau <garm@ilumeo.com>
2020-06-18 17:37:45 +0200

    Remove testing script, cargo test --all should be used instead

--- test.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-cargo test --package parameterized-macro --test tests expected_ok_all -- --exact && \
-cargo test --package parameterized-macro --test tests expected_failures  -- --exact
